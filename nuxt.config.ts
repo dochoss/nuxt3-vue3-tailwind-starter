a5c886869280ab058fc7943faf8f6ed9e4610488
Bo Clifton <boclifton@gmail.com>
2023-03-21 11:32:36 -0500

    switch to nuxt/tailwind module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
+    modules: [
+      '@nuxtjs/tailwindcss'
+    ],
     postcss: {
       plugins: {
         tailwindcss: {},
